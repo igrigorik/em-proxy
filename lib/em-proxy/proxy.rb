5b77e732c0515cbd6e7dea0cfd289549b2c315be
Dominik Sander <git@dsander.de>
2009-12-21 13:13:07 +0100

    Ruby 1.9 compatibility (instance_eval is no longer yielding)

--- lib/em-proxy/proxy.rb
+++ lib/em-proxy/proxy.rb
@@ -9,7 +9,7 @@ class Proxy
 
       EventMachine::start_server(options[:host], options[:port],
                                  EventMachine::ProxyServer::Connection, options) do |c|
-        c.instance_eval(&blk)
+        instance_exec(c, &blk)
       end
     end
   end
