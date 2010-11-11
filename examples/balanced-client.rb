204ad0c49c9f313e2c88ab0ea1192fe7bbfae1b5
Karel Minarik <karmi@karmi.cz>
2010-11-11 19:13:33 +0100

    Added example of a simple load-balancing proxy
    Client to send multiple requests included in examples/balanced-client.rb

---
+++ examples/balanced-client.rb
@@ -0,0 +1,21 @@
+# A simple HTTP client, which sends multiple requests to the proxy server
+
+require 'net/http'
+
+proxy = Net::HTTP::Proxy('0.0.0.0', '9999')
+
+count = ENV['COUNT'] || 5
+
+threads = []
+count.to_i.times do |i|
+  threads << Thread.new do
+    # puts "* Request #{i+1}\n"
+    proxy.start('www.example.com') do |http|
+      puts http.get('/').body
+      puts "^^^ #{i+1} " + '-'*80 + "\n\n"
+    end
+    sleep 0.1
+  end
+end
+
+threads.each { |t| t.join }
