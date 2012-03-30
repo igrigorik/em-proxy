46578cc0ab52e5612d898cebabfb1789bfbc43d0
Jos Boumans <jos@krux.com>
2012-03-30 00:13:22 +0000

    * if a duplex server is down or very slow to respond, that holds up the connection. Add a timeout (default 5 seconds) if you are willing to drop the reply

--- lib/em-proxy/connection.rb
+++ lib/em-proxy/connection.rb
@@ -35,14 +35,19 @@ module EventMachine
         end
 
         servers.each do |s|
+
           s.send_data data unless data.nil?
 
           ### if you do not care about the reply, we will simply
-          ### close the connection to the backend server after we 
+          ### close the connection to the backend server after we
           ### sent the data to it. Great for doing shadow traffic
           ### --jib
+          ### Also, don't wait too long for the connection; if
+          ### the remote isn't there, move on as it's affecting
+          ### the throughput
           if s.drop_reply then
-            debug [:drop_reply, s.name]
+            debug [:drop_reply, s.name, s.connect_timeout]
+            s.pending_connect_timeout = s.connect_timeout
             s.close_connection_after_writing
           end
         end
@@ -53,7 +58,7 @@ module EventMachine
       #
       def server(name, opts)
         srv = EventMachine::connect(opts[:host], opts[:port],
-            EventMachine::ProxyServer::Backend, @debug, opts[:drop_reply]) do |c|
+            EventMachine::ProxyServer::Backend, @debug, opts[:drop_reply], opts[:connect_timeout] ) do |c|
           c.name = name
           c.plexer = self
           c.proxy_incoming_to(self, 10240) if opts[:relay_server]
