eee702db713edd3ebeede8447610df8a61f74653
Konstantin Makarchev <kostya27@gmail.com>
2012-04-29 00:00:59 +0400

    remove another example

--- em-proxy.gemspec
+++ em-proxy.gemspec
@@ -17,6 +17,7 @@ Gem::Specification.new do |s|
   s.add_development_dependency "rspec"
   s.add_development_dependency "em-http-request"
   s.add_development_dependency "ansi"
+  s.add_development_dependency "rake"
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
--- examples/http_proxy.rb
+++ examples/http_proxy.rb
@@ -4,6 +4,7 @@ require 'uuid'        # gem install uuid
 
 # > ruby http_proxy.rb
 # > curl --proxy localhost:9889 www.google.com
+# > curl --proxy x.x.x.x:9889 www.google.com - bind ip example
 
 host = "0.0.0.0"
 port = 9889
@@ -17,7 +18,8 @@ Proxy.start(:host => host, :port => port) do |conn|
     puts "New session: #{session} (#{h.inspect})"
 
     host, port = h['Host'].split(':')
-    conn.server session, :host => host, :port => (port || 80)
+    conn.server session, :host => host, :port => (port || 80) #, :bind_host => conn.peer[0] - # for bind ip
+                         
     conn.relay_to_servers @buffer
 
     @buffer.clear
